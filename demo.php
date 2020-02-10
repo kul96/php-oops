347137eb8235ffa2fbba38b1b9e25427a1af7dec
kul96 <kuldeepsuthar96@gmail.com>
2020-02-10 19:03:12 +0530

    add demo.php file

---
+++ demo.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * 
+ */
+class Demo
+{
+	private $name,$rollno;
+
+    function getData($name,$rollno){
+    	$this->name =  $name;
+    	$this->rollno = $rollno;
+    }
+    
+    function showData(){
+    	echo "name = ".$this->name."\n";
+    	echo "rollno = ".$this->rollno;
+    }
+ 
+}
+
+$obj = new Demo();
+$obj->getData("kuldeep",120);
+$obj->showData();
+
+ ?>
